3333a15b8e18904bc04cc6d0e3a356ebba1d8192
Michał Moskal <mmoskal@UDS.local>
2018-11-10 19:34:37 +0100

    Init

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# canvas-empty-template
+## Empty HTML + Vanilla JS project for Canvas game making.
+#nolibs #canvas #game-template #starter
+
+### Includes:
+    setup(){
+        // ...
+    }
+    update(deltaTime){
+        // ...
+    }
+    render(){
+        // ...
+    }
+
+Canvas resize on windows resize.
---
+++ main.js
@@ -0,0 +1,50 @@
+const PI = Math.PI;
+const canvas = document.querySelector('#mainCanvas');
+const ctx = canvas.getContext("2d");
+
+var resolution = {x: 1920, y: 1080};
+
+function clear(){
+	ctx.fillStyle = "#FFFFFF";
+	ctx.fillRect(0, 0, resolution.x, resolution.y);
+}
+
+function resizeCanvas() {
+	canvas.width = resolution.x = window.innerWidth;
+	canvas.height = resolution.y =  window.innerHeight;
+}
+
+window.addEventListener('resize', function(){
+	resizeCanvas();
+});
+
+function setup(){
+
+}
+
+function update(deltaTime){
+
+}
+
+function render(){
+
+}
+
+const requestAnimationFrame = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame || window.mozRequestAnimationFrame;
+
+var then = performance.now();
+var mainLoop = function () {
+	var now = performance.now();
+	var delta = now - then;
+	update(delta / 1000);
+	render();
+	then = now;
+	requestAnimationFrame(mainLoop);	
+}
+
+document.addEventListener('DOMContentLoaded', function(){
+    resizeCanvas();
+    clear();
+    setup();
+    mainLoop();
+});
